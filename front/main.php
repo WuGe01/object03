668b867134e74237bf0efcc1eb5e39b59bf79a88
吳宗融 <bc781208@hotmail.com>
2020-07-16 11:43:45 +0800

    main大圖切換

--- front/main.php
+++ front/main.php
@@ -59,10 +59,12 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
              echo "<div class='po'>";
              echo "<img src='./img/".$r['path']."'>";
              echo "<div>".$r['name']."</div>";
+             echo "<input type='hidden' name='ani' value='".$r['ani']."'>";
              echo "</div>";
             }
           ?>
         </div>
+       
         <div class="btns">
           <div class="control_btn" onclick="shift(1)">&#9664;</div>
           <div class="nav">
@@ -78,26 +80,77 @@ $rows=$po->all(['sh'=>1]," order by `rank`");
         </div>
       </div>
     </div>  
-  <script>
-    $(".po").eq(0).show()
-    let move=0;
-    let page=$('.icon').length;
-    function shift(e) {
-        switch (e) {
-          case 1:
-            if(move<(page-4)){
-              move++
-              $('.icon').animate({right:move*80})
-            }
-            break;
-          case 2:
-            if(move>0){
-              move--
-              $('.icon').animate({right:move*80})
-            }
-            break;
+<script>
+$(".po").eq(0).show()
+let move=0;
+let page=$('.icon').length;
+
+let auto=setInterval(()=>{
+  slider()
+},3000)
+$(".nav").hover(
+  function () {
+    clearInterval(auto)
+  },
+  function () {
+    auto=setInterval(()=>{slider()},3000)
+  }
+)
+$(".icon").on('click',function () {
+  let num=$('.icon').index($(this))
+  $(".po").hide()
+  $(".po").eq(num).show() 
+  // let ani=$(".po").eq(num).children('input').val();
+  // switch (ani) {
+  //   case "1":
+  //     $(num).fadeIn(1000)
+  //     break;
+  //   case "2":
+  //     $(num).show(1000)
+  //     break;
+  //   case "3":
+  //     $(num).slideDown(1000)
+  //     break;
+  // }
+})
+function slider() {
+  let dom=$(".po:visible");
+  let ani=$(dom).children('input').val();
+  let next=$(dom).next();
+  if(next.length=="0")next=$(".po").eq(0);
+  switch (ani) {
+    case "1":
+      $(dom).fadeOut(2000)
+      $(next).fadeIn(2000)
+      break;
+    case "2":
+      $(dom).hide(1000,()=>{
+        $(next).show(1000)
+      })
+      break;
+    case "3":
+      $(dom).slideUp(1000,()=>{
+        $(next).slideDown(1000)
+      })
+      break;
+  }
+}
+function shift(e) {
+    switch (e) {
+      case 2:
+        if(move<(page-4)){
+          move++
+          $('.icon').animate({right:move*80},500)
         }
+        break;
+      case 1:
+        if(move>0){
+          move--
+          $('.icon').animate({right:move*80},500)
+        }
+        break;
     }
+}
   </script>
     <!-- 院線片區 -->
     <div class="half">
