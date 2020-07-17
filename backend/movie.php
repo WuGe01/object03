2b86c72610bcdcd1b56bdde8ecebc17f8b12ea91
吳宗融 <bc781208@hotmail.com>
2020-07-17 11:51:51 +0800

    api3

--- backend/movie.php
+++ backend/movie.php
@@ -1,20 +1,7 @@
-<style>
-    .list{
-        overflow: auto;
-        width: 95%;
-        background-color: #eee;
-        margin: auto;
-        height: 420px;    
-    }
-    .move_item{
-        height: 130px;
-    }
-</style>
 <div style="width: 80%;height: 480px;margin: auto;background-color: #CCC;padding: 20px;border-radius: 5px;">
     <input type="button" onclick="location.href='?do=add_movice'" value="新增電影">
     <hr>
     <div class="list">
-
     </div>
 </div>
 <script>
